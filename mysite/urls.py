d6d0258ab5e43635970d4a235dccf666587dcf59
kisatouchiha722-oss <kisatouchiha722@gmail.com>
2026-06-12 01:14:45 +0500

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings # Добавь этот импорт
+from django.conf.urls.static import static # И этот импорт
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('polls.urls')),  # Перенаправляем главную страницу на polls
+    path('', include('polls.urls')),
 ]
+
+# И добавь этот блок в самый конец файла:
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
